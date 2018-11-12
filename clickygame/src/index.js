379931a6bd4112dc6da889246a03025cb5ee916c
henman3 <henrymontesanoiii@gmail.com>
2018-11-12 14:39:17 -0500

    Music v2

--- clickygame/src/index.js
+++ clickygame/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Music from './App';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
