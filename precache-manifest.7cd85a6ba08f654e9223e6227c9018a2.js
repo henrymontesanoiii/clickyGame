5dda68eda6e102c4971edd93fec444f54b728a57
henman3 <henrymontesanoiii@gmail.com>
2018-11-13 14:09:25 -0500

    Updates

---
+++ precache-manifest.7cd85a6ba08f654e9223e6227c9018a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "517ed7bf2b447b813967",
+    "url": "/clickygame/static/js/main.517ed7bf.chunk.js"
+  },
+  {
+    "revision": "8802385745528426871f",
+    "url": "/clickygame/static/js/1.88023857.chunk.js"
+  },
+  {
+    "revision": "517ed7bf2b447b813967",
+    "url": "/clickygame/static/css/main.f7e5c371.chunk.css"
+  },
+  {
+    "revision": "2653e8d0718aeeea36af9f6c93a160da",
+    "url": "/clickygame/index.html"
+  }
+];
